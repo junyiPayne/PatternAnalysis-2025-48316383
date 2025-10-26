9e6b1c8664908758a78e39ac2599b17e324ffc8e
Junyi Wen <s4831638l@uq.edu.au>
2025-10-27 01:01:05 +1000

    update new config for 3050Ti

--- recognition/3D_ImprovedUNet_48316383/config.py
+++ recognition/3D_ImprovedUNet_48316383/config.py
@@ -3,7 +3,7 @@ CONFIG = {
     'target_size': (128, 128, 64),  # 图像下采样尺寸
     'batch_size': 2,  # 训练批量大小
     'num_workers': 4,  # 数据加载线程数
-    'base_filters': 32,  # UNet3D 的基础通道数
+    'base_filters': 16,  # UNet3D 的基础通道数
     'val_freq': 1,  # 每隔多少个 epoch 验证一次
     'grad_accum_steps': 1,  # 梯度累积步数
     'use_preload': False,  # 是否预加载数据
